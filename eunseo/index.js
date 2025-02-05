acbf7fa60013078ab34f542f9f46a3485187a079
EUNSEO78 <dmstjalstj4@gmail.com>
2025-02-06 01:36:02 +0900

    modify

--- eunseo/index.js
+++ eunseo/index.js
@@ -12,6 +12,30 @@ const linkUrlList = [
     id: "trendyHyundai",
     url: "https://www.hyundai.com/kr/ko/e/service-membership/ev/hi-ev",
   },
+  {
+    id: "en",
+    url: "https://www.hyundai.com/kr/en/catalog_price/hydrogen_ev",
+  },
+  {
+    id: "cn",
+    url: "https://www.hyundai.com/kr/en/catalog_price/hydrogen_ev",
+  },
+  {
+    id: "worldWide",
+    url: "https://www.hyundai.com/worldwide/ko#utm_source=hmc-kr&utm_medium=referral&utm_campaign=top_util",
+  },
+  {
+    id: "global",
+    url: "https://trucknbus.hyundai.com/global/en/",
+  },
+  {
+    id: "perLogin",
+    url: "https://idpconnect-kr.hyundai.com/auth/api/v2/user/oauth2/authorize?client_id=2b6b3d2d-5ccc-497f-8e8b-e9b2d7582097&redirect_uri=https%3A%2F%2Fwww.hyundai.com%2Fkr%2Fko%2Fccspcallback.html%3Fhmgid%3D2&response_type=code&scope=&state=login&connector_client_id=hmgid1.0-2b6b3d2d-5ccc-497f-8e8b-e9b2d7582097&ui_locales=&connector_scope=&connector_session_key=dca02db6-f693-48b9-bd02-a26c1988cd1d",
+  },
+  {
+    id: "corLogin",
+    url: "https://www.hyundai.com/kr/ko/corporation-login",
+  },
 ];
 
 [...linkUrlList].forEach((v) => {
@@ -42,8 +66,7 @@ close_btn.addEventListener("click", () => {
   navs__wrap.style.setProperty("display", "block");
 });
 
-// language 와 user => hover
 // 스크롤 내리면 header 위로 올라감
-// 스크롤 올리면 header 아래로 내려옴
 // 스크롤 내리면 바가 채워짐
+// 스크롤 올리면 header 아래로 내려옴
 // 스크롤 올리면 바가 다시 위로 감
