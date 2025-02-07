9d0f5e94737cab2bf257a2c82fb37408dcfe1aca
jiheon <wwlgjs77@gmail.com>
2025-02-07 10:11:14 +0900

    ..

--- main/js/index.js
+++ main/js/index.js
@@ -1,9 +1,11 @@
 import { electronicCarArr, NcarArr, passengerCarArr, suvArr } from "./array.js";
 import { carCardCreat } from "./card.js";
 import { closedetail } from "./close.js";
+import { footeropen } from "./footerlistopen copy.js";
 
 carCardCreat(electronicCarArr, electronicCarList1);
 carCardCreat(NcarArr, Ncar);
 carCardCreat(passengerCarArr, passengerCar);
 carCardCreat(suvArr, SUV);
 closedetail();
+footeropen();
