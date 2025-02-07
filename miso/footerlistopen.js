97d444862204e14c4054c5cac9b1044b7851976f
kimmiso <kmiiiso916@naver.com>
2025-02-07 17:00:40 +0900

    aaa

--- miso/footerlistopen.js
+++ miso/footerlistopen.js
@@ -1,8 +1,6 @@
-export const footeropen = () => {
-  const lower_box = document.querySelector(".lower_box");
-  lower_box.addEventListener("click", () => {
-    const lower_box_list = document.querySelector(".lower_box_list");
-    lower_box_list.style.display =
-      lower_box_list.style.display == "block" ? "none" : "block";
-  });
-};
+const lower_box = document.querySelector(".lower_box");
+lower_box.addEventListener("click", () => {
+  const lower_box_list = document.querySelector(".lower_box_list");
+  lower_box_list.style.display =
+    lower_box_list.style.display == "block" ? "none" : "block";
+});
